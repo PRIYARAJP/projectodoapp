6af12dd96b55a49193cf01009a39442f0c477b1d
PRIYADHARSHINI R <122624837+PRIYARAJP@users.noreply.github.com>
2023-04-14 14:12:11 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,4 +1,7 @@
 
+let popcar =document.getElementById("apper") 
+popcar.style.display="none"
+/* for first popup */
 function additem(){
     let popcar =document.getElementById("apper") 
     popcar.style.display="none"
@@ -12,10 +15,6 @@ function additem(){
     
     // let backgr =document.getElementById("opacitych")  
     
-    head.style.opacity=.1;
-    head.innerHTML=`<span class="bold" >TASKS</span> LIST
-    <spam class="add">
-        <span class="circle" onclick="additem()" id="additem">+</span> Add Item</spam>          `;
     pop1.innerHTML=`
     <nav  class="addview"  >
     <nav>Add New List</nav><br>
@@ -30,7 +29,7 @@ function additem(){
     rowsdone.innerHTML=child;
     
 }
-
+/*get a value in popup and view that in cords */
 let data=[];
 
 function card1(){
@@ -39,19 +38,22 @@ function card1(){
     head.style.opacity=1;
     let cards = document.getElementById("card")
     cards.style.opacity=1;
-    let newtex = document.getElementById("texts").value;
+    let newtex = document.getElementById("texts").value;// get value
+    /* create value id and convert it to string */
     let item = {
         id: new Date().getTime(),
         title: newtex,
       };
         if (newtex) {
+            /* convert the string as array */
         data.push(item);
+        /* for new cord */
         addui();
-        // addwinback();
         } else  {
+            /* if you don't give the value this is generated */
         alert("Please  enter the Card Title ☺️");
          }
-
+        /* condition for fux display cord */
          let pop1 =document.getElementById("todono")
             
          let rowsdone =document.getElementById(`additem`)    
@@ -63,25 +65,27 @@ function card1(){
              pop1.style.display="none"
              
          }
+            /* create cords */
          function addui() {
            
             let popcards =document.getElementById("card") 
             popcards.style.display="flex"
+            
+
+
             let coadop ="";  
                   
   for ( let index = 0; index < data.length; index ++) {
 
     if(index <= data.length){
-console.log(data[index].title);
-// console.log("l "+data.length);
-// console.log(index);
 titl =data[index].title;
 id1=data[index].id;
-
+let nn = titl
+// .replaceAll(" ","mmmmmmm");
 
 coadop =`
 <nav class="cards" id="cd${data[index].id}">
-<p id="${titl}" onclick="openwindow(${id1},${titl})"> ${data[index].title}</p><hr>
+<p id="${titl}" onclick="openwindow(${id1},${nn})" value="5" id="n${id1}">${nn}</p><hr>
 <p id="addtk${data[index].id}"><p>
 <span class="c1 c2" id="del${data[index].id}"  onclick="delcard(${data[index].id})">
 d</span>
@@ -96,134 +100,72 @@ d</span>
 }
 
 }
-
-
-
-
+/* open new window click on heading tag  */
 
 function openwindow(id,tit){
-    console.log(id,tit.id);
+    let a=5;
     let head = document.getElementById("head")
-    // head.style.display="none";
-    // let backbutton =document.getElementById("back") 
-    // head.innerHTML="hii"
-    head.innerHTML=` <span class="flexdir"><span  onclick="backcard(${id})">back</span><span class="newwin">${tit.id}</span><span class="circle" onclick="additem()"  id="additem">+</span></span>
+
+    head.innerHTML=` <span class="flexdir"><span  onclick="backcard(${id})">back
+    </span><span class="newwin" id="newwind">${tit.id}</span>
+    <span class="circle" onclick="adddwin(${id})" id="additem">+</span></span>
      `;
 
-     
      let popcar =document.getElementById("apper") 
-     popcar.style.display="flex"
+     popcar.style.display="block"
      let cardId = `cd${id}`;
      let card =document.getElementById(cardId);
-     card.parentNode.isEqualNode(card);
+     card.parentNode.isSameNode(card);
+    const mii=card.innerHTML;
+    
+     popcar.innerHTML =mii;    
+     
+   
      data = data.filter(item => item.id != id);
-     popcar.innerHTML=`<samp class="cards" id=cdss${id}>${card.innerHTML}</samp>`;    
-    
-    
-    
-     //  card.innerHTML=popcar.innerHTML
-    //  coadop =`
-    // <nav class="cards" id="cd${id}"><p id="${tit}" onclick="openwindow(${id},${tit})"> ${tit.id}</p><hr>
-    // <p id="addtk${id}"><p>
-    // <span class="c1 c2" id="del${id}"  onclick="delcard(${id})">
-    // d</span>
-    // <span class="c1" id="${id}" id="addtask${id}" onclick="addtask(${id})">+</span></nav>`   
-    
-    // popcar.innerHTML = coadop
-
-    // let po =document.getElementById("card") 
-    // po.style.display="none"
+    let po =document.getElementById("card") 
+    po.style.display="none"
+    console.log(a);
 }
 
-function backcard(id){
-    
+/* adding new cord and update details from heading page  */
+function adddwin(id){
+
     let head = document.getElementById("head")
+     head.innerHTML=`<span class="bold" >TASKS</span> LIST
+    <spam class="add">
+        <span class="circle" onclick="additem()" id="additem">+</span> Add Item</spam>`
     head.style.opacity=1;
     let cards = document.getElementById(`cd${id}`)
     cards.style.opacity=1;
     let popcar =document.getElementById("apper") ;
-    cards.innerHTML=`<nav >${popcar.innerHTML}</nav>`
+    cards.innerHTML=popcar.innerHTML
+    popcar.innerHTML="";
 
-    console.log(popcar);
-    // popcar.style.display="flex"
+    let po =document.getElementById("card") 
+    po.style.display="flex"   
+    additem();
+    
+}
+
+/* back and update changes */
+function backcard(id){
+ 
+    let head = document.getElementById("head")
+
+    head.innerHTML=`<span class="bold" >TASKS</span> LIST
+    <spam class="add">
+        <span class="circle" onclick="additem()" id="additem">+</span> Add Item</spam>`
+    head.style.opacity=1;
+    let cards = document.getElementById(`cd${id}`)
+    cards.style.opacity=1;
+    let popcar =document.getElementById("apper");
+    cards.innerHTML=popcar.innerHTML;
     let po =document.getElementById("card") 
     po.style.display="flex"
-//     const cardId = `cd${id}`;
-// const card =document.getElementById(cardId);
-// card.parentNode.removeChild(card);
-// data = data.filter(item => item.id != id);
-
-// card.parentNode.appendChild(popcar);
+    popcar.style.display="none"
 }   
 
-
-
-
-function back(id){
-    let head = document.getElementById("head")
-    head.innerHTML=`<span class="bold" >TASKS</span> LIST<span  onclick="backcard(${id})">back</span>
-    <spam class="add">
-        <span class="circle" onclick="additem()" id="additem">+</span> Add Item</spam> `;
-        let popcar =document.getElementById("apper") 
-
-    //  popcar.style.display="none"
-     const cardId = `cd${id}`;
-     const card =document.getElementById(cardId);
-     card.parentNode.replaceChild(card);
-     data = data.filter(item => item.id != id);
-     card.pop(popcar)
-}
-
-function addwinback() {
-        
-    let popcards =document.getElementById("card") 
-    let coadop ="";  
-          
-for ( let index = 0; index < data.length; index ++) {
-
-if(index <= data.length){
-console.log(data[index].title);
-// console.log("l "+data.length);
-// console.log(index);
-titl =data[index].title;
-id1=data[index].id;
-coadop =`
-<nav class="cards" id="cd${data[index].id}"><p id="${titl}" onclick="openwindow(${id1},${titl})"> ${data[index].title}</p><hr>
-<p id="addtk${data[index].id}"><p>
-<span class="c1 c2" id="del${data[index].id}"  onclick="delcard(${data[index].id})">
-d</span>
-<span class="c1" id="${data[index].id}" id="addtask${data[index].id}" onclick="addtask(${data[index].id})">+</span></nav>`
-}
-
-}
-
-popcards.innerHTML =`<a>${coadop}</a>`
-
-}
-
-
-function addnewein(taskid){
-    let coaditem="";
- for ( let i = 0; i < da.length; i++) {
-console.log("fffffffffffffff"+taskid);
-    if(i <= da.length){           
-coaditem +=`<p id="${da[i].id}${taskid}">
-<span id="new${da[i].id}${taskid}">
-${da[i].tit}<span>
-<span class="makerd" id="makeread${da[i].id}${taskid}"
- onclick="makeread(${da[i].id},${taskid})">Make as read</span>
- </p>`  
-
-    }
-   
-}
-add.innerHTML = coaditem; 
-
- }
-
-
-
-
+/* delete cord  */
 function delcard(id){
     let head = document.getElementById("head")
     head.style.opacity=1;
@@ -236,18 +178,18 @@ data = data.filter(item => item.id != id);
 
 }
 
+/* popup2 for update task update task inside cord */
 function addtask(taskid){   
+    let popcarer =document.getElementById("apper") 
+    popcarer.style.display="none"
     let head = document.getElementById("head")  
-    head.innerHTML=`<span class="bold" >TASKS</span> LIST
-    <spam class="add">
-        <span class="circle" onclick="additem()" id="additem">+</span> Add Item</spam> `;
+    
     head.style.opacity=.1;
     let cards = document.getElementById("card")
     cards.style.opacity=.1;
 let add = document.getElementById(`${taskid}`)
 let pop1 =document.getElementById("todono")  
 pop1.style.display="flex"
-    console.log(taskid);
 pop1.innerHTML =`
     <nav  class="addview" id="newtripcd">
     <nav>Add New Task</nav><br>
@@ -258,9 +200,7 @@ pop1.innerHTML =`
 </nav>`
 }
 
-
-
-
+/* for cancel the popup1 and 2  */
 function cancel(){
     
     let head = document.getElementById("head")
@@ -271,16 +211,38 @@ function cancel(){
     pop1.style.display="none"
 }
 
+/* for insert task inside card */
 const da=[];
 function cardstask(taskid){
+   
+    let newval =document.getElementById("n${id1}") 
+    console.log("vvvvvvvvvvvvvvvvvvv"+newval);
+    let poc =document.getElementById("newwind")
+// console.log("wwwddddddddddd"+poc);
+/* condition for view cord */
+    if(poc){
+        let popcarer =document.getElementById("apper") 
+        popcarer.style.display="block"
+        let cards = document.getElementById("card")
+        cards.style.opacity=1;
+       
+    }
+    else if(poc == null) {
+        let popcarer =document.getElementById("apper") 
+        popcarer.style.display="none"
+        let cards = document.getElementById("card")
+        cards.style.opacity=1;
+    }
+
     let head = document.getElementById("head")
     head.style.opacity=1;
-    let cards = document.getElementById("card")
-    cards.style.opacity=1;
+    
+    
     let add = document.getElementById(`addtk${taskid}`)
     let newtext = document.getElementById("textsfortask").value;
     let pop1 =document.getElementById("todono")  
     pop1.style.display="none"
+    /* data convert as string */
     const it ={
         id : new Date().getTime().toString(),
         tit:newtext,     
@@ -288,24 +250,18 @@ function cardstask(taskid){
     
 
     if (newtext||it.tit) {
-      
+      /* string convert as array */
         da.push(it);  
-       
+       /* insert task */
     additem(taskid);
       
     } else  {
     alert("Please  enter the Item ☺️");
      }
-     
+     /* update task */
      function additem(taskid){
         let coaditem="";
      for ( let i = 0; i < da.length; i++) {
-        // let head = document.getElementById("head")
-        // head.style.opacity=1;
-        // let cards = document.getElementById(`cd${taskid}`)
-        // cards.style.opacity=1;
-        // let popcar =document.getElementById("apper") ;
-        // cards.innerHTML=`<nav >${popcar.innerHTML}</nav>`
         if(i <= da.length){           
     coaditem =`<p id="${da[i].id}${taskid}">
     <span id="new${da[i].id}${taskid}">
@@ -313,29 +269,24 @@ function cardstask(taskid){
     <span class="makerd" id="makeread${da[i].id}${taskid}"
      onclick="makeread(${da[i].id},${taskid})" >Make as read</span>
      </p>`  
-   
+  
         }
        
     }
-    add.innerHTML +=coaditem; 
+    
+    add.innerHTML += coaditem; 
     
      }
-    
+   
 }
-    
+/* after read a  task make as reed  */
 function makeread(id,taskid){
     let mr = document.getElementById(`makeread${id}${taskid}`)
     mr.style.display="none"
   let tk = document.getElementById(`new${id}${taskid}`)
-
+  let jk = document.getElementById(`n${taskid}`)
   tk.style.color="red"
   tk.style.textDecoration="line-through"
-
-  
-  let popcar =document.getElementById("apper") ;
-  tk.innerHTML=`<nav >${tk.innerHTML}</nav>`
-
-
   data = data.filter(item => item.id != id);
-//   da = da.filter(it => it.id != id);
 }
+
